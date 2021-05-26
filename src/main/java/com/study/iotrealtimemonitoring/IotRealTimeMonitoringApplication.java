5f1cbfadef440cc6f9b50f0df427f217aba6aafd
홍창의 <redcrow4@gmail.com>
2021-05-26 20:15:20 +0900

    Initial commit

---
+++ src/main/java/com/study/iotrealtimemonitoring/IotRealTimeMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.study.iotrealtimemonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotRealTimeMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotRealTimeMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/iotrealtimemonitoring/IotRealTimeMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.iotrealtimemonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotRealTimeMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
